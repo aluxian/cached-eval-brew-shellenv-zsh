5b7775b14a5a928f01df9bfecf1267e42101ee55
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-03-26 11:43:09 +0200

    Simplify plugin loading
    * Auto-call both functions, so that end users no longer have to specify them in the `zinit` statement. (Zinit was renamed from `zplg` or `zplugin`. See https://github.com/zdharma/zinit/commit/f7786298eee30999cd9ab6e50b967d23b6bd1fbf ) 
    * Update the Readme with the new `zinit` syntax.
    * Move the cache file to the conventional `~/.cache` directory and change its extension to `.zsh`
    * Use `.` instead of `_` as the function name prefix. See https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc#standard-function-name-space-prefixes

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 Cached `eval $(brew shellenv)` optimised for speed.
 
 ```zsh
-zplg ice lucid atpull'_clear_cache_eval_brew_shellenv' atload'_cached_eval_brew_shell'
-zplg light aluxian/cached-eval-brew-shellenv-zsh
+zinit light aluxian/cached-eval-brew-shellenv-zsh
 ```
 
 ## License
--- cached-eval-brew-shellenv.plugin.zsh
+++ cached-eval-brew-shellenv.plugin.zsh
@@ -1,9 +1,12 @@
-function _cached_eval_brew_shell() {
-  local CACHE_FILE_PATH=/tmp/brew-shellenv-cache.txt
+function .cached_eval_brew_shell() {
+  local CACHE_FILE_PATH=~/.cache/brew-shellenv.zsh
   eval $([ -f $CACHE_FILE_PATH ] && cat $CACHE_FILE_PATH || brew shellenv | tee $CACHE_FILE_PATH)
 }
 
-function _clear_cache_eval_brew_shellenv() {
-  local CACHE_FILE_PATH=/tmp/brew-shellenv-cache.txt
+function .clear_cache_eval_brew_shellenv() {
+  local CACHE_FILE_PATH=~/.cache/brew-shellenv.zsh
   rm -rf $CACHE_FILE_PATH
 }
+
+@zsh-plugin-run-on-update .clear_cache_eval_brew_shellenv
+.cached_eval_brew_shell "$@"
